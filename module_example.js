ba64c17ed3293e033972e7d4701d25ea863e481f
노승은 <seun2230@gmail.com>
2021-09-08 10:19:41 +0900

    Modules

--- module_example.js
+++ module_example.js
@@ -18,3 +18,19 @@ console.log(process.env);       // 환경 정보
 // Global 변수
 console.log(__dirname);    // 현재 모듈의 디렉터리
 console.log(__filename);   // 현재 모듈의 파일명
+
+// 모듈로부터 개별 객체 불러오기
+// const  add = require("./modules/test_module1").add;  // # 모듈 불러올 시 js파일명 생략함.
+// const square = require("./modules/test_module1").square;
+
+// 전개 연산을 이용한 require
+const { add, square } = require("./modules/test_module1");     // require로 불러온 것과 {}의 내용과 일치해야 성립함
+const area = require("./modules/test_module2");
+console.log(add(10,20));    // 불러온 객체 사용
+console.log(square(30));
+
+console.log(area.square(40));
+console.log(area.rectangle(10, 20));
+console.log(area.circle(30));
+
+// # node.js에서는 require 사용, ES6에서는 import를 사용하여 데이터를 내보낸다.
---
+++ modules/test_module1.js
@@ -0,0 +1,7 @@
+// exports 객체: 모듈을 내보내는 전역 객체
+exports.add = (num1, num2) => {
+    return num1 + num2;
+}  // add 라는 이름으로 객체 내보냄
+exports.square = (length) => {
+    return length * length;
+}
